c90f591dbe84a510652ab6c32c6ab38b0a0346bd
MikevPeeren <MikevPeeren@hotmail.com>
2020-11-17 13:26:10 +0100

    :sparkles: made sure dots are replaced by spaces

--- src/messages/environments.ts
+++ src/messages/environments.ts
@@ -3,6 +3,12 @@ import { stagingInUseBy, featureOneInUseBy, featureTwoInUseBy } from '../data/en
 // This will match any message that contains
 export const getEnvironmentStatus = async ({ message, say }: any) => {
   await say(
-    `Hello, <@${message.user}>. Staging is in use by ${stagingInUseBy}, Feature One is in use by ${featureOneInUseBy}, Feature Two is in use by ${featureTwoInUseBy}`,
+    `Hello, <@${message.user}>. Staging is in use by ${stagingInUseBy.replace(
+      /\./g,
+      ' ',
+    )}, Feature One is in use by ${featureOneInUseBy.replace(
+      /\./g,
+      ' ',
+    )}, Feature Two is in use by ${featureTwoInUseBy.replace(/\./g, ' ')}`,
   );
 };
